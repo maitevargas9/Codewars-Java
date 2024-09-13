a35522ba6960c6751448d3d65a9552e4e82a2748
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2024-09-13 12:29:17 +0200

    First commit

---
+++ 8kyu/Remove exclamation marks.java
@@ -0,0 +1,12 @@
+/*
+Description
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+class Solution {
+
+  static String removeExclamationMarks(String s) {
+    return s.replace("!", "");
+  }
+}
---
+++ 8kyu/You Can't Code Under Pressure #1.java
@@ -0,0 +1,12 @@
+/*
+Description
+
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+class Java {
+
+  public static int doubleInteger(int i) {
+    return i * 2;
+  }
+}
